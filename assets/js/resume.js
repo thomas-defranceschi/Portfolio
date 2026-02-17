b131e17bf744b0e2c80ba0872b3d57cc3d9960b2
Hemriel <153784566+Hemriel@users.noreply.github.com>
2026-02-17 11:45:27 +0900

    Enhance resume section: add button IDs for improved functionality and update active state handling

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -3,17 +3,30 @@ function showResume(type) {
   const long = document.getElementById("resume-long");
   const download = document.getElementById("download");
 
+  const short_btn = document.getElementById("short-btn");
+  const long_btn = document.getElementById("long-btn");
+  const down_btn = document.getElementById("down-btn");
+
   if (type === "short") {
     short.style.display = "block";
     long.style.display = "none";
-    download.style.display = "none"
+    download.style.display = "none";
+    short_btn.classList.add("active");
+    long_btn.classList.remove("active");
+    down_btn.classList.remove("active");
   } else if (type === "long") {
     short.style.display = "none";
     long.style.display = "block";
     download.style.display = "none";
+    short_btn.classList.remove("active");
+    long_btn.classList.add("active");
+    down_btn.classList.remove("active");
   } else {
     short.style.display = "none";
     long.style.display = "none";
     download.style.display = "block";
+    short_btn.classList.remove("active");
+    long_btn.classList.remove("active");
+    down_btn.classList.add("active");
   }
 }
