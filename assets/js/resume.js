3118eff199a3f3f2a42116bfc841bd912986891e
Hemriel <153784566+Hemriel@users.noreply.github.com>
2026-02-17 11:40:26 +0900

    Fix resume button functionality: update download button to trigger download view

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,12 +1,19 @@
 function showResume(type) {
   const short = document.getElementById("resume-short");
   const long = document.getElementById("resume-long");
+  const download = document.getElementById("download");
 
   if (type === "short") {
     short.style.display = "block";
     long.style.display = "none";
-  } else {
+    download.style.display = "none"
+  } else if (type === "long") {
     short.style.display = "none";
     long.style.display = "block";
+    download.style.display = "none";
+  } else {
+    short.style.display = "none";
+    long.style.display = "none";
+    download.style.display = "block";
   }
 }
